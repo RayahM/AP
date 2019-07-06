1d171b4a138a0966bd20d772032cf153484ed2eb
rezaneo7 <ramazanpoorreza@gmail.com>
2019-07-05 17:04:52 -0700

    edit some image

--- Backgommon/src/sample/Controller.java
+++ Backgommon/src/sample/Controller.java
@@ -1,13 +1,16 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,13 +26,61 @@ public class Controller implements Initializable {
    static double middle = 73;
    static double offset_y = 562;
 
-   private Image image = new Image(getClass().getResourceAsStream("spritesheet.png"));
-
+   private Image imageDice1 = new Image(getClass().getResourceAsStream("spritesheet.png"));
+    private Image imageDice2 = new Image(getClass().getResourceAsStream("spritesheetR.png"));
+   private int spriteColumns = 3;
+   private int spriteCounts = 6;
+   private int spriteOffsetX = 0;
+   private int spriteOffsetY = 0;
+   private int spriteWidth = 69;
+   private int spriteHeight = 70;
 
 
     @FXML
     void rollDice(MouseEvent event) {
+        ImageView imageViewDice1 = new ImageView(imageDice1);
+        imageViewDice1.setTranslateX(350);
+        imageViewDice1.setTranslateY(260);
 
+        ImageView imageViewDice2 = new ImageView(imageDice2);
+        imageViewDice2.setTranslateX(420);
+        imageViewDice2.setTranslateY(260);
+        imageViewDice1.setViewport(new Rectangle2D(spriteOffsetX,spriteOffsetY,spriteWidth,spriteHeight));
+        imageViewDice2.setViewport(new Rectangle2D(spriteOffsetX,spriteOffsetY,spriteWidth,spriteHeight));
+        Animation animationDice1 = new SpriteAnimation(
+                imageViewDice1,
+                Duration.millis(1500),
+                spriteCounts,
+                spriteColumns,
+                spriteOffsetX,
+                spriteOffsetY,
+                spriteWidth,
+                spriteHeight
+        );
+
+        Animation animationDice2 = new SpriteAnimation(
+                imageViewDice2,
+                Duration.millis(1500),
+                spriteCounts,
+                spriteColumns,
+                spriteOffsetX,
+                spriteOffsetY,
+                spriteWidth,
+                spriteHeight
+        );
+
+        animationDice1.setCycleCount(1);
+        animationDice1.play();
+        animationDice2.setCycleCount(1);
+        animationDice2.play();
+        animationDice1.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                boardPane.getChildren().remove(imageViewDice1);
+                boardPane.getChildren().remove(imageViewDice2);
+            }
+        });
+        boardPane.getChildren().addAll(imageViewDice1,imageViewDice2);
     }
 
    @Override
